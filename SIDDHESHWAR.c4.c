55dd0c72fde91c58b1f376c3b3cd585b8bdf7e09
siddhu-birajdar <birajdarsiddheshwar635@gmail.com>
2024-10-07 22:08:54 -0700

    Add files via upload

---
+++ SIDDHESHWAR.c4.c
@@ -0,0 +1,30 @@
+//program to check given no. is even or odd
+#include<stdio.h>
+
+int main(){
+    int number;
+
+    printf("enter the number");
+    scanf("%d",&number);
+
+    if (number%2==0)
+    {
+        printf("\n even number ");
+
+    }
+
+    else
+    {
+        printf("\n odd number");
+    }
+
+
+
+
+
+
+
+
+
+
+}
