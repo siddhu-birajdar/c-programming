98cc42067c5adc03b042dfc77117e35ed94189c0
siddhu-birajdar <birajdarsiddheshwar635@gmail.com>
2024-10-07 22:20:08 -0700

    Update siddheshwar.c5.c

--- siddheshwar.c5.c
+++ siddheshwar.c5.c
@@ -8,7 +8,7 @@ int main(){
     printf("enter the alphabet:");
     scanf("%c",&alphabet);
 
-    if(alphabet=='a'||alphabet=='e'||alphabet=='i'||alphabet=='o'||alphabet=='u')
+    if(alphabet=='a'||alphabet=='e'||alphabet=='i'||alphabet=='o'||alphabet=='u'||alphabet=='A'||alphabet=='E'||alphabet=='I'||alphabet=='O'||alphabet=='U')
     {
         printf("\n it is vowel");
 
