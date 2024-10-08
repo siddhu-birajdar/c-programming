4ceaee4c060a7ce70656c0b8b693cf8aff8712bb
siddhu-birajdar <birajdarsiddheshwar635@gmail.com>
2024-10-07 22:10:56 -0700

    Add files via upload

---
+++ siddheshwar.6.c
@@ -0,0 +1,54 @@
+//pragram to give the bonus for employe workingbfor three years
+#include<stdio.h>
+
+int main()
+    {
+        int joining_year,current_year;
+
+        printf("enter the employe's joning_year");
+        scanf("%d",&joining_year);
+
+        printf("enter the  employe's current_year");
+        scanf("%d",&current_year);
+
+        if (current_year - joining_year < 3)
+        {
+            printf("\n you will not get bonus");
+
+        }
+        else
+        {
+            printf("\n you will get bonus 2500");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
