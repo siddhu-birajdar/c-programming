c174a3fc4e102bfb0f1d1e49285c343e97c46bf5
siddhu-birajdar <birajdarsiddheshwar635@gmail.com>
2024-10-07 22:09:27 -0700

    Add files via upload

---
+++ siddheshwar.c5.c
@@ -0,0 +1,58 @@
+//program to check whether given char is vowel or consonant
+#include<stdio.h>
+
+int main(){
+
+    char alphabet;
+
+    printf("enter the alphabet:");
+    scanf("%c",&alphabet);
+
+    if(alphabet=='a'||alphabet=='e'||alphabet=='i'||alphabet=='o'||alphabet=='u')
+    {
+        printf("\n it is vowel");
+
+    }
+    else
+    {
+        printf("\n it is consonant");
+        printf("\n it is consonant");
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
