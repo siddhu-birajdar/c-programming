e00c35b4d68faa188434bbb297bb6b0675ea532b
siddhu-birajdar <birajdarsiddheshwar635@gmail.com>
2024-10-22 05:30:12 -0700

    Update siddheshwar16..c

--- siddheshwar16..c
+++ siddheshwar16..c
@@ -1,28 +1,58 @@
 //program for star pattern using while loop
+#include <stdio.h>
 
 
-#include<stdio.h>
 
 int main()
+
 {
-    int i=0,j,r;
-    printf("enter the no. of rows");
-    scanf("%d",&r);
 
+    int i=1, j, r;
 
-    while (i<=r)
+    printf("Enter the number of rows: ");
+
+    scanf("%d", &r);
+
+    while (i <= r)
+
     {
-        j=1;
-        while(j<=r-i)
 
-            while (i<=r)
-    {
-        j=1;
-        while(j<=i)
+        j = 1;
+
+        while (j <= i)
+
         {
+
             printf("*");
+
             j++;
+
         }
+
         printf("\n");
+
         i++;
+    }
+       i=j-1;
+
+
+       while (i<=r)
+
+    {
+       j=1;
+
+        while (j <=r-i)
+
+        {
+
+            printf("*");
+
+            j++;
+         }
+
+        printf("\n");
+
+        i++;
+
+    }
     }
